e7fde3b3f7012bc7d3bbfdf89fb4eec5ff2a60f0
kvezal <muckam49@mail.ru>
2019-05-12 06:20:43 +0300

    Проработана анимация корневых роутов

--- src/app/ad.animations.ts
+++ src/app/ad.animations.ts
@@ -2,18 +2,43 @@ import {animate, group, query, style, transition, trigger} from "@angular/animat
 import {RouteList} from "./ad.interface";
 
 export const adRouteAnimations = trigger('adRouteAnimations', [
-  transition(`${RouteList.CSS} => ${RouteList.SVG_ANIMATION}`, toggleRoute(RouteList.CSS)),
+  transition(`${RouteList.CSS} => ${RouteList.SVG_ANIMATION}`, toggleRoute(`${RouteList.CSS}To${RouteList.SVG_ANIMATION}`)),
+  transition(`${RouteList.CSS} => ${RouteList.JS_ANIMATION}`, toggleRoute(`${RouteList.CSS}To${RouteList.JS_ANIMATION}`)),
+  transition(`${RouteList.SVG_ANIMATION} => ${RouteList.CSS}`, toggleRoute(`${RouteList.SVG_ANIMATION}To${RouteList.CSS}`)),
+  transition(`${RouteList.SVG_ANIMATION} => ${RouteList.JS_ANIMATION}`, toggleRoute(`${RouteList.SVG_ANIMATION}To${RouteList.JS_ANIMATION}`)),
+  transition(`${RouteList.JS_ANIMATION} => ${RouteList.CSS}`, toggleRoute(`${RouteList.JS_ANIMATION}To${RouteList.CSS}`)),
+  transition(`${RouteList.JS_ANIMATION} => ${RouteList.SVG_ANIMATION}`, toggleRoute(`${RouteList.JS_ANIMATION}To${RouteList.SVG_ANIMATION}`)),
   transition(`* => ${RouteList.CSS}, * => ${RouteList.SVG_ANIMATION}, * => ${RouteList.JS_ANIMATION}`, showRoute()),
-  // transition(`${RouteList.CSS} => ${RouteList.JS_ANIMATION}`, toggleRoute(RouteList.CSS)),
 ]);
 
-function toggleRoute(route: RouteList = RouteList.CSS) {
+function toggleRoute(route: string) {
   const optional = { optional: true };
-  /*const adRouteTransformOrigin = {
-    [RouteList.CSS]: '375px 0',
-    [RouteList.SVG_ANIMATION]: '545px 0',
-    [RouteList.JS_ANIMATION]: '700px 0',
-  };*/
+  const adRouteTransformOrigin = {
+    [`${RouteList.CSS}To${RouteList.SVG_ANIMATION}`]: {
+      hidden: '375px 0',
+      showing: '545px 0',
+    },
+    [`${RouteList.CSS}To${RouteList.JS_ANIMATION}`]: {
+      hidden: '375px 0',
+      showing: '700px 0',
+    },
+    [`${RouteList.SVG_ANIMATION}To${RouteList.CSS}`]: {
+      hidden: '545px 0',
+      showing: '375px 0',
+    },
+    [`${RouteList.SVG_ANIMATION}To${RouteList.JS_ANIMATION}`]: {
+      hidden: '545px 0',
+      showing: '700px 0',
+    },
+    [`${RouteList.JS_ANIMATION}To${RouteList.CSS}`]: {
+      hidden: '700px 0',
+      showing: '375px 0',
+    },
+    [`${RouteList.JS_ANIMATION}To${RouteList.SVG_ANIMATION}`]: {
+      hidden: '700px 0',
+      showing: '545px 0',
+    },
+  };
 
   return [
     group([
@@ -26,36 +51,24 @@ function toggleRoute(route: RouteList = RouteList.CSS) {
           left: 0,
           opacity: 1,
         }),
-        animate('2s ease', style({ transform: 'scale(1)' }))
-      ]),
+      ], optional),
       query(':enter', [
-        style({ transform: 'scale(0)' }),
+        style({
+          transformOrigin: adRouteTransformOrigin[route].showing,
+          transform: 'scale(0)',
+        }),
         animate('2s ease', style({ transform: 'scale(1)' }))
       ]),
       query(':leave', [
-        animate('2s ease', style({ transform: 'scale(0)' }))
+        style({
+          transformOrigin: adRouteTransformOrigin[route].hidden,
+        }),
+        animate('2s ease', style({
+          transform: 'scale(0)',
+        }))
       ])
     ])
-  ]
-  /*return [
-    query(
-      ':enter',
-      [
-        style({
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          opacity: 1,
-          transformOrigin: adRouteTransformOrigin[route],
-          transform: 'scale(0)',
-        }),
-        animate('2000ms ease', style({ transform: 'scale(1)' }))
-      ],
-      optional
-    )
-  ];*/
+  ];
 }
 
 function showRoute(route: RouteList = RouteList.CSS) {
